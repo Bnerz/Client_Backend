60c565712f77591f906d2e71aae2edc8bade5a06
Abner Teixeira <abner.ba81@gmail.com>
2021-12-08 19:46:01 +0000

    created repository, services, resources

--- client/src/main/java/com/desafio1/client/resources/ClientResource.java
+++ client/src/main/java/com/desafio1/client/resources/ClientResource.java
@@ -2,26 +2,28 @@ package com.desafio1.client.resources;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.time.Instant;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio1.client.entities.Client;
+import com.desafio1.client.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-	List<Client> list = new ArrayList<>();
-	list.add(new Client(1L, "Paulo", "123456789-53", 1000.00,Instant.parse("2021-07-14T00:00:00Z"), 2));
-	return ResponseEntity.ok().body(list);
-	
-	
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+
 	}
-	
+
 }
